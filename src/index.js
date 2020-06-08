35ba09bf023e4156abd30e8a916d8123e706d274
malikH88 <malik.hyamine@gmail.com>
2020-06-08 17:07:04 +0200

    resolvegit add .git add .

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import lightReducer from './lightReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  lightReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
